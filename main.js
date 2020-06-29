a54022ae32d31eb54a98b5605c714675b158a3fe
Myra <myrakornides@gmail.com>
2020-06-29 15:13:54 -0400

    dog images appear for selected breed √

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// VARIABLES -- (const are generally in all caps (things unlikely to change))
+const breedsListElement = document.querySelector('#dogBreedsList')
+const thumnailContainerElement = document.querySelector('#thumbnailContainerElement')
+const IMAGE_COUNT = 50;
+const PAGINATE_BY = 10;
+
+const breedsInputElement = document.querySelector('#dogBreedInput')
+
+// FUNCTIONS
+
 async function getDogBreeds() {
   let response = await fetch('https://dog.ceo/api/breeds/list/all');
   let breeds = await response.json();
@@ -6,7 +16,6 @@ async function getDogBreeds() {
 
 async function dogBreedOptions() {
   const breeds = await getDogBreeds()
-  const breedsListElement = document.querySelector('#dogBreedsList')
   for (const breed of breeds) {
     const optionElement = document.createElement('option');
     optionElement.setAttribute('value', breed);
@@ -15,3 +24,26 @@ async function dogBreedOptions() {
 }
 
 dogBreedOptions();
+
+async function getDogImages(breed, count) {
+  let response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/${count}`)
+  let images = await response.json();
+  return images.message
+}
+
+async function onDogBreedSelected(event) {
+  const breed = event.target.value;
+  const images = await getDogImages(breed, IMAGE_COUNT);
+
+  function populateThumbnails(start) {
+    thumnailContainerElement.innerHTML = '';
+    for (const image of images.slice(start, start + PAGINATE_BY)) {
+      const imageElement = document.createElement('div');
+      imageElement.innerHTML = `<img src=${image}>`
+      thumbnailContainerElement.appendChild(imageElement)
+    }
+  }
+  populateThumbnails(0)
+}
+
+breedsInputElement.addEventListener('change', onDogBreedSelected);
