9bfd6d5192e4fba2b572647d2dc6c90ef1749933
Myra <myrakornides@gmail.com>
2020-06-29 21:38:32 -0400

    css and linting √

--- main.js
+++ main.js
@@ -1,89 +1,107 @@
-// VARIABLES -- (const are generally in all caps (things unlikely to change))
-const IMAGE_COUNT = 50;
-const PAGINATE_BY = 10;
+(function () {
+  // VARIABLES
+  const IMAGE_COUNT = 50;
+  const PAGINATE_BY = 10;
 
-const breedsListElement = document.querySelector('#dogBreedsList')
-const thumnailContainerElement = document.querySelector('#thumbnailContainerElement')
-const breedsInputElement = document.querySelector('#dogBreedInput')
-const paginationElement = document.querySelector('#paginationElement')
-const imageModal = document.querySelector('#imageModal')
-const imageModalDisplay = document.querySelector('#imageModalDisplay')
+  const dogBreedsList = document.querySelector('#dogBreedsList');
+  const thumbnailContainerElement = document.querySelector(
+    '#thumbnailContainerElement'
+  );
+  const dogBreedInput = document.querySelector('#dogBreedInput');
+  const paginationElement = document.querySelector('#paginationElement');
+  const imageModal = document.querySelector('#imageModal');
+  const imageModalDisplay = document.querySelector('#imageModalDisplay');
 
+  // API FUNCTIONS
 
-// FUNCTIONS
-
-async function getDogBreeds() {
-  let response = await fetch('https://dog.ceo/api/breeds/list/all');
-  let breeds = await response.json();
-  return Object.keys(breeds.message);
-}
-
-async function dogBreedOptions() {
-  const breeds = await getDogBreeds()
-  for (const breed of breeds) {
-    const optionElement = document.createElement('option');
-    optionElement.setAttribute('value', breed);
-    breedsListElement.appendChild(optionElement)
+  async function getDogBreeds() {
+    let response = await fetch('https://dog.ceo/api/breeds/list/all');
+    let breeds = await response.json();
+    return Object.keys(breeds.message);
   }
-}
 
-dogBreedOptions();
+  async function getDogImages(breed, count) {
+    let response = await fetch(
+      `https://dog.ceo/api/breed/${breed}/images/random/${count}`
+    );
+    let images = await response.json();
+    return images.message;
+  }
 
-async function getDogImages(breed, count) {
-  let response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/${count}`)
-  let images = await response.json();
-  return images.message
-}
+  // FUNCTIONS
 
-async function updateBreed(breed) {
-  const images = await getDogImages(breed, IMAGE_COUNT);
+  async function setupDogBreedOptions() {
+    const breeds = await getDogBreeds();
+    for (const breed of breeds) {
+      const optionElement = document.createElement('option');
+      optionElement.setAttribute('value', breed);
+      dogBreedsList.appendChild(optionElement);
+    }
+  }
 
-  function populateThumbnails(start) {
-    thumnailContainerElement.innerHTML = '';
+  function populateThumbnails(images, start) {
+    thumbnailContainerElement.innerHTML = '';
+
     for (const image of images.slice(start, start + PAGINATE_BY)) {
       const imageElement = document.createElement('div');
-      imageElement.innerHTML = `<img src=${image}>`
-      thumbnailContainerElement.appendChild(imageElement)
-      imageElement.addEventListener("click", function () {
+      imageElement.innerHTML = `<img src=${image}>`;
+      thumbnailContainerElement.appendChild(imageElement);
+      imageElement.addEventListener('click', function () {
         displayModal(image);
       });
     }
   }
 
-  paginationElement.innerHTML = '';
-  for (let pageNumber = 0; pageNumber < images.length / 10; pageNumber += 1) {
-    const pageNumberElement = document.createElement('button');
-    pageNumberElement.innerHTML = `${pageNumber + 1}`
-    pageNumberElement.addEventListener('click', function (event) {
-      let oldActivePage = document.querySelector("#paginationElement button.active");
-      if (oldActivePage) {
-        oldActivePage.classList.remove("active");
+  async function changeSelectedBreed(breed) {
+    const images = await getDogImages(breed, IMAGE_COUNT);
+
+    paginationElement.innerHTML = '';
+
+    for (let pageNumber = 0; pageNumber < images.length / 10; pageNumber += 1) {
+      const pageNumberElement = document.createElement('button');
+      if (pageNumber === 0) {
+        pageNumberElement.classList.add('active');
       }
-      event.target.classList.add("active");
-      populateThumbnails(pageNumber * 10)
-    })
-    paginationElement.appendChild(pageNumberElement);
+      pageNumberElement.innerHTML = `${pageNumber + 1}`;
+      pageNumberElement.addEventListener('click', function (event) {
+        let oldActivePage = document.querySelector(
+          '#paginationElement button.active'
+        );
+        if (oldActivePage) {
+          oldActivePage.classList.remove('active');
+        }
+        event.target.classList.add('active');
+        populateThumbnails(images, pageNumber * 10);
+      });
+      paginationElement.appendChild(pageNumberElement);
+    }
+    populateThumbnails(images, 0);
   }
-  populateThumbnails(0)
-}
 
-function onDogBreedSelected(event) {
-  const breed = event.target.value;
-  updateBreed(breed)
-}
+  function displayModal(image) {
+    imageModalDisplay.setAttribute('src', image);
+    imageModal.style.visibility = 'visible';
+  }
 
-function displayModal(image) {
-  imageModalDisplay.setAttribute('src', image);
-  imageModal.style.visibility = 'visible';
-}
+  function hideModal() {
+    imageModal.style.visibility = 'hidden';
+  }
 
-function hideModal() {
-  imageModal.style.visibility = 'hidden';
-}
+  // EVENT HANDLERS
 
+  function onDogBreedSelected(event) {
+    const breed = event.target.value;
+    changeSelectedBreed(breed);
+  }
 
-breedsInputElement.addEventListener('change', onDogBreedSelected);
-imageModal.addEventListener("click", hideModal);
+  // INIT
 
+  function init() {
+    setupDogBreedOptions();
 
-updateBreed("beagle"); // just for testing
+    dogBreedInput.addEventListener('change', onDogBreedSelected);
+    imageModal.addEventListener('click', hideModal);
+  }
+
+  init();
+})();
